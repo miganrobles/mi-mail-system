6443ad857da2ff1f02608010b48d047371c7610e
miguel <miganrobles@gmail.com>
2015-11-23 20:18:58 +0100

    Codificada la clase MailClient, creando atributos, métodos y constructor que se nos pide en la práctica

--- MailClient.java
+++ MailClient.java
@@ -23,5 +23,52 @@
  */
 public class MailClient
 {
-   
+    // Este atributo representa el servidor de correo
+    private MailServer server;
+    // Este atributo representa la dirección de correo del usuario que usa ese cliente
+    private String user;
+
+    /**
+     * Constructor para objetos de la clase MailClient que permite crear un objeto MailClient inicializando sus atributos 
+     * por medio de parámetros que serán el nombre del servidor(tipo MailServer) y el nombre del usuario(tipo String).
+     */
+    public MailClient(MailServer mailServidor, String mailUsuario)
+    {
+        server = mailServidor;
+        user = mailUsuario;
+    }
+    
+    /**
+     * Este método recupera del servidor el correo del ususario.
+     */ 
+    public MailItem getNextMailItem()
+    {
+        return server.getNextMailItem(user);
+    }
+    
+    /**
+     * Método printNextMailItem que recupera del servidor el siguiente correo que tenga el usuario e imprime por 
+     * pantalla los datos de dicho mensaje. Si no hay ningun mensaje, muestra un mensaje por pantalla informando de ello.
+     */
+    public void printNextMailItem()
+    {
+        MailItem email = getNextMailItem();
+        if (email != null) {
+            email.print();
+        }
+        else {
+            System.out.println("No tiene ningún mensaje nuevo.");
+        }
+    }
+    
+    /**
+     * Método sendMailItem que recibe 2 parámetros (un String indicando para quién es el mensaje y otro 
+     * String indicando el contenido del mensaje), 
+     * crea un email (objeto MailItem) basándose en la información de dichos parámetros y lo envía al servidor asociado a ese cliente.
+     */
+    public void sendMailItem(String para, String mensaje)
+    {
+        MailItem email = new MailItem(user, para, mensaje);
+        server.post(email);
+    }
 }
