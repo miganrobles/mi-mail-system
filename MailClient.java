d78231836dc61f058f570433a5c0b8986e926d50
miguel <miganrobles@gmail.com>
2015-12-11 11:58:20 +0100

    Modificados métodos getNexMailItem y printNextMailItem para que detecten spam y obren en consecuencia como se pide

--- MailClient.java
+++ MailClient.java
@@ -3,18 +3,16 @@
 
  *Disponga de 2 atributos: uno de tipo MailServer llamado server (que representa el servidor asociado con ese cliente) y otro de tipo String 
  *llamado user (que representa la dirección de correo del usuario que usa ese cliente).
-   
+
  *Disponga de un constructor que permita crear un objeto MailClient inicializando sus atributos por medio de parámetros.
-    
+
  *Disponga de un método llamado getNextMailItem que recupere del servidor el siguiente correo (un objeto MailItem) que tenga el usuario y devuelva dicho objeto.
-    
+
  *Disponga de un método llamado printNextMailItem que recupere del servidor el siguiente correo (un objeto MailItem) que tenga el usuario e imprima por 
  *pantalla los datos de dicho mensaje. Si no hay ningun mensaje, que muestre un mensaje por pantalla informando de ello.
-    
+
  *Disponga de un método llamado sendMailItem que reciba 2 parámetros (un String indicando para quién es el mensaje y otro String indicando el contenido del mensaje), 
  *cree un email (objeto MailItem) basándose en la información de dichos parámetros y lo envíe al servidor asociado a ese cliente.
-
-
 
  * Write a description of class MailClient here.
  * 
@@ -29,6 +27,8 @@ public class MailClient
     private String user;
     // Atributo donde se almacena el último mensaje
     private MailItem buzon;
+    // Este atributo nos va a almecenar si el correo tiene spam
+    private boolean spam;
 
     /**
      * Constructor para objetos de la clase MailClient que permite crear un objeto MailClient inicializando sus atributos 
@@ -39,20 +39,32 @@ public class MailClient
         this.server = server;
         this.user = user;
         buzon = null;
+        spam = false;
     }
-    
+
     /**
      * Este método recupera del servidor el correo del ususario.
      */ 
     public MailItem getNextMailItem()
     {
         MailItem email = server.getNextMailItem(user);
-        if (email!= null) {
-            buzon = email;
+        if (email != null) {
+            if (email.getMessage().contains("trabajo")) {
+                spam = false;
+                buzon = email;
+            }
+            else if (email.getMessage().contains("regalo") || email.getMessage().contains("promocion")) {
+                spam = true;
+                email = null;
+            }
+            else  {
+                spam = false;
+                buzon = email;
+            }
         }
         return email;
     }
-    
+
     /**
      * Este método imprime por pantalla el número de mensajes que tiene el usuario en el servidor sin leer
      */
@@ -60,7 +72,7 @@ public class MailClient
     {
         System.out.println("Correos sin leer en el servidor: " + server.howManyMailItems(user));
     }
-    
+
     /**
      * Este método obtiene del servidor un mensaje para el ususario y envía una respuesta automática
      */
@@ -71,7 +83,7 @@ public class MailClient
             sendMailItem(email.getFrom(),"RE: " + email.getSubject() , "Estoy en la oficina.\n" +  email.getMessage());
         }
     }
-    
+
     /**
      * Método printNextMailItem que recupera del servidor el siguiente correo que tenga el usuario e imprime por 
      * pantalla los datos de dicho mensaje. Si no hay ningun mensaje, muestra un mensaje por pantalla informando de ello.
@@ -82,11 +94,14 @@ public class MailClient
         if (email != null) {
             email.print();
         }
+        else if (spam == true) {
+            System.out.println("Este correo contiene spam");
+        }
         else {
             System.out.println("No tienes ningún mensaje");
         }
     }
-    
+
     /**
      * Método sendMailItem que recibe 2 parámetros (un String indicando para quién es el mensaje y otro 
      * String indicando el contenido del mensaje), 
@@ -97,7 +112,7 @@ public class MailClient
         MailItem email = new MailItem(user, para, asunto, mensaje);
         server.post(email);
     }
-    
+
     /**
      * Este método nos imprime por pantalla el último mensaje recibido
      */
@@ -112,4 +127,3 @@ public class MailClient
         }
     }
 } 
-   
